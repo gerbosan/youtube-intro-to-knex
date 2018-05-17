1881e6d113c513125f5d66c842f6422cfc20a293
Carlos A <ikari@EVA03.NERV>
2018-05-16 19:59:12 -0500

    v0.0.2: Normalizando salida de datos, corrección de Bug en innerJoin()

--- readme.md
+++ readme.md
@@ -1,4 +1,18 @@
 # youtube-intro-to-knex
-EL código original se puede encontrar en [Github: johnazre - Youtube intro to knex](https://github.com/johnazre/youtube-intro-to-knex), código para los videos en [Youtube: KnexJS with Express and Node Tutorial](https://www.youtube.com/playlist?list=PL7sCSgsRZ-smPRSrim4bX5TQfRue1jKfw). Se encontrarán con algunos problemas con el archivo knexfile.js que no se corrigió adecuadamente, pero los datos están aquí: [Github - Knex migrate and seed CLI commands throwing error - Issue#2000: tgriesser/knex](https://github.com/tgriesser/knex/issues/2000)
+El código original se puede encontrar en [Github: johnazre - Youtube intro to knex](https://github.com/johnazre/youtube-intro-to-knex), código para los videos en [Youtube: KnexJS with Express and Node Tutorial](https://www.youtube.com/playlist?list=PL7sCSgsRZ-smPRSrim4bX5TQfRue1jKfw). Se encontrarán con algunos problemas con el archivo knexfile.js que no se corrigió adecuadamente, pero los datos están aquí: [Github - Knex migrate and seed CLI commands throwing error - Issue#2000: tgriesser/knex](https://github.com/tgriesser/knex/issues/2000)
+
+## Tabla USERS
+|id|name|email|created_at|updated_at|
+|:-:|:-:|:-:|:-:|:-:|
+
+## Tabla TODOS
+|id|title|completed|user_id|created_at|updated_at|
+|:-:|:-:|:-:|:-:|:-:|:-:|
+
+## Ramas
 
 **v0.0.1B:** Comenzaré a crear una versión alternativa, ordenando el código, usando mysql y Typescript. Sin embargo aún (2018-05-11), no dispongo conocimiento si Knex puede trabajar adecuadamente con Typescript. 
+
+**v0.0.2:** Configurando para una adecuada salida de datos, añadiendo estados. La información está basada en un proyecto anterior: [Github: API REST usando NodeJS y Typescript](https://github.com/gerbosan/node-restful-api-tutorial). También se normaliza para que todos los procedimientos retornen los mismos datos, lista completa de TODOS, excepto las opciones de filtrado por *todos.id* y *users.id*. 
+
+**Bug:** Procedimiento para listar TODOS filtrados por user_id, resultado aparece con todo_id mostrando el user_id. El problema parece pertener a los datos que devuelve Knex. Se solucionó controlando los campos que devuelve Knex.
--- src/api/controllers/todosControllers.ts
+++ src/api/controllers/todosControllers.ts
@@ -1,63 +1,207 @@
 import { Request, Response, NextFunction } from 'express';
 const knex = require('knex')(require('../../conf/knexfile.js'));
 const controller = {
-    listar: (req: Request, res: Response, next: NextFunction) => {
+    listar: async (req: Request, res: Response, next: NextFunction) => {
+        /*
         knex.select()
             .from('todos')
             .then( todos => {
                 res.send(todos);
+            })*/
+        knex.column('todos.id', 'todos.title', 'todos.completed', 'todos.updated_at', 'todos.user_id', 'users.name', 'users.email')
+            .from('todos')
+            .innerJoin('users', 'todos.user_id', 'users.id')
+            .then(data => {
+                // res.send(data);
+                /*
+                const response = {
+                    count: data.length,
+                    todos: data.map(todo => {
+                        return {
+                            todo_id: todo['id'],
+                            todo_title: todo['title'],
+                            todo_completed: todo['completed'],
+                            todo_updated: todo['updated_at'],
+                            todo_userId: todo['user_id'],
+                            todo_userName: todo['name'],
+                            todo_userEmail: todo['email'],
+                            request: {
+                                type: 'GET',
+                                url: '/todos/' + todo['id']
+                            }
+                        }
+                    })
+                }*/
+
+                if(data.length >=1) {
+                    res
+                        .status(200)
+                        .json(listResults(data));
+                } else {
+                    res
+                        .status(404)
+                        .json({
+                            message: 'No entries found'
+                        });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res
+                    .status(500)
+                    .json({
+                        error: err
+                    })
             })
     },
 
-    filtrar: (req: Request, res: Response, next: NextFunction) => {
+    filtrar: async (req: Request, res: Response, next: NextFunction) => {
         const user_id: number = req.params.id;
-        knex.from('todos')
+        knex.column('todos.id', 'todos.title', 'todos.completed', 'todos.updated_at', 'todos.user_id', 'users.name', 'users.email')
+            .from('todos')
             .innerJoin('users', 'todos.user_id', 'users.id')
             .where('todos.user_id', user_id)
             .then(data => {
-                res.send(data);
-            });
+                // res.send(data);
+                /*
+                const response = {
+                    count: data.length,
+                    todos: data.map(todo => {
+                        return {
+                            todo_id: todo['id'],
+                            todo_title: todo['title'],
+                            todo_completed: todo['completed'],
+                            todo_updated: todo['updated_at'],
+                            todo_userId: todo['user_id'],
+                            todo_userName: todo['name'],
+                            todo_userEmail: todo['email'],
+                            request: {
+                                type: 'GET',
+                                url: '/todos/' + todo['id']
+                            }
+                        }
+                    })
+                }*/
+                if(data.length >= 1) {
+                    res
+                        .status(200)
+                        .json(listResults(data));
+                } else {
+                    res
+                        .status(404)
+                        .json({
+                            message: 'No entries found' 
+                        });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({error: err})
+            })
     },
 
-    filtrar_2: (req: Request, res: Response, next: NextFunction) => {
+    filtrar_2: async (req: Request, res: Response, next: NextFunction) => {
         const todo_id: number = req.params.id;
-        knex.select()
+        knex.column('todos.id', 'todos.title', 'todos.completed', 'todos.updated_at', 'todos.user_id', 'users.name', 'users.email')
             .from('todos')
-            .where('id', todo_id)
-            .then( todos => {
-                res.send(todos);
-            });
+            .innerJoin('users', 'todos.user_id', 'users.id')
+            .where('todos.id', todo_id)
+            .then( data => {
+                // res.send(data);
+                /*
+                const response = {
+                    count: data.length,
+                    todos: data.map(todo => {
+                        return {
+                            todo_id: todo['id'],
+                            todo_title: todo['title'],
+                            todo_completed: todo['completed'],
+                            todo_updated: todo['updated_at'],
+                            todo_userId: todo['user_id'],
+                            todo_userName: todo['name'],
+                            todo_userEmail: todo['email'],
+                            request: {
+                                type: 'GET',
+                                url: '/todos/' + todo['id']
+                            }
+                        }
+                    })
+                }*/
+                if(data.length >= 1) {
+                    res.status(200).json(listResults(data));
+                } else {
+                    res.status(404).json({ message: 'No entries found' });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({error: err})
+            })
     },
 
-    nuevo: (req:Request, res: Response, next: NextFunction) => {
+    nuevo: async (req:Request, res: Response, next: NextFunction) => {
         const title:string = req.body.title;
         const user_id:number = req.body.user_id;
 
         knex('todos').insert({ title, user_id })
             .then(() => {
+                /* Devuelve lista de TODOS
                 knex.select()
                     .from('todos')
                     .then((todos) => {
                         res.send(todos);
                     });
-            });
+                */
+               knex.column('todos.id', 'todos.title', 'todos.completed', 'todos.updated_at', 'todos.user_id', 'users.name', 'users.email')
+                    .from('todos')
+                    .innerJoin('users', 'todos.user_id', 'users.id')
+                    .then(data => {
+                        if(data.length >= 1) {
+                            res.status(201).json(listResults(data));
+                        } else {
+                            res.status(404).json({ message: 'No entries found' });
+                        }
+                    })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ error: err})
+                
+            })
     },
 
     editar: (req: Request, res: Response, next: NextFunction) => {
         const title: string = req.body.title;
-        const completed: boolean = req.body.completed;        
+        const completed: boolean = req.body.completed;
         const todo_id: number = req.params.id;
 
         knex('todos')
             .where('id', todo_id)
             .update({ title, completed })
             .then(() => {
+                /*
                 knex.select()
                     .from('todos')
                     .then((todos) => {
                         res.send(todos);
                     });
-            });
+                */
+               knex.column('todos.id', 'todos.title', 'todos.completed', 'todos.updated_at', 'todos.user_id', 'users.name', 'users.email')
+                   .from('todos')
+                   .innerJoin('users', 'todos.user_id', 'users.id')
+                   .then(data => {
+                       if(data.length >= 1) {
+                           res.status(200).json(listResults(data));
+                       } else {
+                           res.status(404).json({message: 'No entries found'});
+                       }
+                   })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({error: err})
+                
+            })
     },
 
     borrar: (req: Request, res: Response, next: NextFunction) => {
@@ -66,12 +210,52 @@ const controller = {
             .where('id', todo_id)
             .del()
             .then(() => {
+                /*
                 knex.select()
                     .from('todos')
                     .then((todos) => {
                         res.send(todos);
                     });
+                */
+               knex.column('todos.id', 'todos.title', 'todos.completed', 'todos.updated_at', 'todos.user_id', 'users.name', 'users.email')
+                    .from('todos')
+                    .innerJoin('users', 'todos.user_id', 'users.id')
+                    .then(data => {
+                        if(data.length >= 1) {
+                            res.status(200).json(listResults(data));
+                        } else {
+                            res.status(404).json({message: 'No entries found'});
+                        }
+                    })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({error: err});
+                
             });
     }
 };
+
+function listResults(info) {
+    const response = {
+        count: info.length,
+        todos: info.map(todo => {
+            return {
+                todo_id: todo['id'],
+                todo_title: todo['title'],
+                todo_completed: todo['completed'],
+                todo_updated: todo['updated_at'],
+                todo_userId: todo['user_id'],
+                todo_userName: todo['name'],
+                todo_userEmail: todo['email'],
+                request: {
+                    type: 'GET',
+                    url: '/todos/' + todo['id']
+                }
+            }
+        })
+    }
+    return response;
+}
+
 export { controller };
